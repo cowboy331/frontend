190bc4c3987da6057bb1b39af065465fda9b7b1a
cowboy331 <cowboy331@gmail.com>
2022-10-10 17:43:33 +0800

    20221010 17:43

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -6,9 +6,48 @@
 
 // var count=1;
 // while (count<11) {
-//     alert(count);
-//     count++;
-// }
+    //     alert(count);
+    //     count++;
+    // }
+    
+function addLoadEvent() {
+    var oldload=window.onload;
+    if (typeof window.onload!='function') {
+        window.onload=func;
+    }
+    else {
+        window.onload=function(){
+            oldload();
+            func();
+        }
+    }
+}
+    
+function insertAfter(newElement,targetElement) {
+    var parent=targetElement.parentNode;
+    if(parent.lastChild==targetElement) {
+        parent.appendChild(newElement);
+    }
+    else{
+        parent.insertBefore(newElement,targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var placeholder=document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.gif");
+    placeholder.setAttribute("alt","my image gallery");
+    var description= document.createElement("p");
+    description.setAttribute("id","description");
+    var gallery=document.getElementById("imagegallery");
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+}
 
 function prepareGallery(){
     if(!document.getElementsByTagName) return false;
@@ -40,7 +79,7 @@ function showPic(whichpic) {
         // }
         description.firstChild.nodeValue=text;
     }
-    return true;
+    return false;
 }
 
 
@@ -50,8 +89,20 @@ function countBodyChildren() {
     alert(body_element.childNodes.length); 
 }
 
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
 
 
-window.onload=countBodyChildren;
-    // window.onload=showPic;
-    
+window.onload=function() {
+    var para=document.createElement("p");
+    var txt1=document.createTextNode("This is ");
+    var emphasis=document.createElement("em");
+    var txt2=document.createTextNode("my");
+    var txt3=document.createTextNode(" content.");
+    para.appendChild(txt1);
+    emphasis.appendChild(txt2);
+    para.appendChild(emphasis);
+    para.appendChild(txt3);
+    var testdiv=document.getElementById("testdiv");
+    testdiv.appendChild(para);
+}
